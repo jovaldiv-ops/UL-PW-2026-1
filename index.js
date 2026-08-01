0cd652b92cdafc556912b9a9a0ea65d2dddc49ed
Pepe Valdivia <pepe.valdivia.caballero@gmail.com>
2026-07-31 19:25:20 -0500

    agrengado index.js para vercel

---
+++ index.js
@@ -0,0 +1,11 @@
+// index.js
+import express from "express";
+import "dotenv/config";
+
+import bootstrap from "../configs/bootstrap.js";
+
+const app = express();
+
+bootstrap(app);
+
+export default app;
