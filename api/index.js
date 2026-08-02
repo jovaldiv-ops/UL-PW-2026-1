8e8e444e975a203ceb3b5a70e37c925856acccde
Pepe Valdivia <pepe.valdivia.caballero@gmail.com>
2026-08-02 08:03:49 -0500

    app para vercel

--- api/index.js
+++ api/index.js
@@ -1,12 +1,4 @@
-import express from 'express';
-import 'dotenv/config';
-import bootstrap from '../configs/bootstrap.js';
+// api/index.js
+import app from '../app.js';
 
-// Creamos la app Express
-const app = express();
-
-// Aplicamos toda la configuración de bootstrap
-bootstrap(app);
-
-// Exportamos la app para Vercel
 export default app;
